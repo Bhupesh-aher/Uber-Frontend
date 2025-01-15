03e6365250a56ad0564941165dbffaed334abf51
Bhupesh-aher <aherbhupesh18@gmail.com>
2025-01-15 15:01:12 +0530

    Home Page

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./components/Home"
+import UserLogin from "./components/UserLogin"
+import UserSignUp from "./components/UserSignUp"
+import CaptainLogin from "./components/CaptainLogin"
+import CaptainSignUp from "./components/CaptainSignUp"
+
+
+function App() {
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<UserLogin/>}/>
+        <Route path="/signup" element={<UserSignUp/>}/>
+        <Route path="/captain-login" element={<CaptainLogin/>}/>
+        <Route path="/captain-signup" element={<CaptainSignUp/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
